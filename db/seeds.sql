45edb765995311612e902954c5090b6c88a0eaca
naritchaya louis <naritchayalouis@gmail.com>
2020-05-01 05:33:58 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Asian Turkey Burgers', 'true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Portobello Burger with Onion Jam', 'true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Grilled Cheeseburgers', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Perfect Salmon Burgers', 'true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Lentil Burgers', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Middle Eastern Chicken Burgers', 'true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jerk Turkey Burgers with Mango Slaw', 'true');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Edamame Veggie Burger', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spiced Burgers with Cucumber Yogurtr', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon-Apple Burgers', 'false');
+
