828a3f5ed6803cb4a98b537df55189a55dac68c7
naritchaya louis <naritchayalouis@gmail.com>
2020-05-05 21:04:17 -0500

    changed app deployment

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,31 @@
 var mysql = require("mysql");
 require("dotenv").config()
 
+var connection = mysql.createConnection({
+host: process.env.HOST,
+port: 3306,
+user: process.env.USER,
+password: process.env.PASSWORD,
+database: process.env.DATABASE
 
-var connection; 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else{
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "6963",
-    database: "burgers_db"
+});
 
-    //process.env.password
+// var connection; 
+// if(process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else{
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "6963",
+//     database: "burgers_db"
+
+//     //process.env.password
   
   
-   });
- }
+//    });
+//  }
 
 connection.connect(function(err) {
   if (err) {
@@ -43,19 +51,15 @@ module.exports = connection;
 
 
 
-// var connection = mysql.createConnection(
+
+
+
+
+
+
 //   process.env.JAWSDB_URL ||{
 //   host: "localhost",
 //   port: 3306,
 //   user: process.env.USER,
 //   password: process.env.PASSWORD,
 //   database: process.env.DATABASE
-// });
-
-
-
-//     // host: process.env.HOST,
-//     // port: 3306,
-//     // user: process.env.USER,
-//     // password: process.env.PASSWORD,
-//     // database: process.env.DATABASE
