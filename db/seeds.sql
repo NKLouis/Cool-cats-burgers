28906f42517b77a08e10f96cc6ba3140d15cb09d
naritchaya louis <naritchayalouis@gmail.com>
2020-05-08 17:31:40 -0500

    changed all true to false

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 -- Insert a set of records.
-INSERT INTO burgers (burger_name, devoured) VALUES ('Asian Turkey Burgers', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Portobello Burger with Onion Jam', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Asian Turkey Burgers', false );
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Portobello Burger with Onion Jam', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Grilled Cheeseburgers', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Perfect Salmon Burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Perfect Salmon Burgers', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Lentil Burgers', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Middle Eastern Chicken Burgers', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Jerk Turkey Burgers with Mango Slaw', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Middle Eastern Chicken Burgers', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jerk Turkey Burgers with Mango Slaw', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Edamame Veggie Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Spiced Burgers with Cucumber Yogurt', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon-Apple Burgers', false);
